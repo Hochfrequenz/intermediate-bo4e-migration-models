80d710814107b9df97e388f8bffa18f8abc4a794
Fstei <frederik.steinkamp@hochfrequenz.de>
2023-09-15 11:54:04 +0200

    1

--- src/bommbee/custom/__init__.py
+++ src/bommbee/custom/__init__.py
@@ -2,7 +2,7 @@
 Bundles customized BO4E models since BO4E is originally designed for market communication not for handling
 customer data etc..
 """
-from powercloud.custom.enum import (
+from bommbee.custom.enum import (
     Abgabeart,
     BDEWArtikelnummerErweitert,
     BoTypErweitert,
