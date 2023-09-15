a7542f474543d036aed533fc202da6222034352d
Fstei <frederik.steinkamp@hochfrequenz.de>
2023-09-15 11:27:30 +0200

    Add BO

---
+++ src/powercloud/custom/bo/__init__.py
@@ -0,0 +1,14 @@
+"""
+Bundles extended BOs
+"""
+from poweercloud.custom.bo import (
+    Bilanzierung,
+    Dokument,
+    File,
+    GeschaeftspartnerErweitert,
+    Hinweis,
+    Kampagne,
+    MarktlokationErweitert,
+    RechnungErweitert,
+    ZaehlerErweitert,
+)
