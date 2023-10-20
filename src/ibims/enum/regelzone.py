8bd4c31e99acc8ba27a2a09b0b0a90b65cf1cddd
Freddy <100769900+FreddyFox892@users.noreply.github.com>
2023-10-20 10:48:35 +0200

    Add Marktgebiet for Gas with Enums (#62)

--- src/ibims/enum/regelzone.py
+++ src/ibims/enum/regelzone.py
@@ -6,11 +6,21 @@ from bo4e.enum.strenum import StrEnum
 
 class Regelzone(StrEnum):
     """
-    a strenum where the str-value of each member is the ILN of the Regelzone
+    a strenum where the str-value of each member is the ILN of the Regelzone for electricity
     """
 
     TRANSNETBW = "10YDE-ENBW-----N"
     TENNETDE = "10YDE-EON------1"
     AMPRION = "10YDE-RWENET---I"
     FUENFZIGHERTZ = "10YDE-VE-------2"
-    GAS = "37Y005053MH0000R"
+
+
+class Marktgebiet(StrEnum):
+    """
+    a strenum where the str-value of each member is the ILN of the Regelzone for Gas
+    Also known as: Marktgebietscode
+    """
+
+    TRADINGHUBEUROPE = "37Y005053MH0000R"
+    THE_H_GAS = "THE0BFH027950000"
+    THE_L_GAS = "THE0BFL027960000"
