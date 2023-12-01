d7df8cf24dba955adf5043d59c74e434235f1c66
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2023-12-01 18:13:47 +0100

    Add workflow to automatically keep BO4E-version up-to-date

---
+++ bo4e/bo4e_updater.py
@@ -0,0 +1,53 @@
+"""
+This script checks if the current BO4E version is up-to-date.
+"""
+from functools import lru_cache
+
+import click
+import requests
+from dotenv import dotenv_values, set_key
+
+OWNER = "Hochfrequenz"
+REPO = "BO4E-Schemas"
+TIMEOUT = 10  # in seconds
+DOTENV_FILE = "bo4e/tox.env"
+
+
+@lru_cache(maxsize=1)
+def resolve_latest_version() -> str:
+    """
+    Resolve the latest BO4E version from the github api.
+    """
+    response = requests.get(f"https://api.github.com/repos/{OWNER}/{REPO}/releases/latest", timeout=TIMEOUT)
+    response.raise_for_status()
+    return response.json()["tag_name"]
+
+
+@lru_cache(maxsize=1)
+def current_version() -> str:
+    """
+    Query the current version from the tox.env file
+    """
+    config = dotenv_values(DOTENV_FILE)
+    return config["BO4E_VERSION"]
+
+
+@click.command()
+def main():
+    """
+    Check if the current version is up-to-date. If so, exit with exit code 0.
+    If not, update the version in the tox.env file and exit with exit code 1.
+    """
+    latest_version = resolve_latest_version()
+    current = current_version()
+    if latest_version == current:
+        print(f"Version {current} is up to date.")
+        return
+
+    # Update BO4E:
+    set_key(DOTENV_FILE, "BO4E_VERSION", latest_version)
+    exit(1)
+
+
+if __name__ == "__main__":
+    main()
