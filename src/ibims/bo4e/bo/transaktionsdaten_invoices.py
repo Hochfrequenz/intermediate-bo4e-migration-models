2c6af0b816827342a5e1e83c10ae81928f2022a4
Stefan Heese <stefan.heese@hochfrequenz.de>
2024-07-05 14:01:48 +0200

    Add mutual fields from Transaktionsdaten

--- src/ibims/bo4e/bo/transaktionsdaten_invoices.py
+++ src/ibims/bo4e/bo/transaktionsdaten_invoices.py
@@ -1,6 +1,6 @@
-from typing import Any
-
 from pydantic import BaseModel, ConfigDict, Field
+
+from ..enum.sparte import Sparte
 
 
 class TransaktionsdatenInvoices(BaseModel):
@@ -12,7 +12,16 @@ class TransaktionsdatenInvoices(BaseModel):
         extra="allow",
         populate_by_name=True,
     )
+    migration_id: str | None = Field(default=None, title="Migration_id")
+    import_fuer_storno_adhoc: str = Field(..., title="Import_fuer_storno_adhoc")
+    sparte: Sparte = Field(..., title="Sparte")
+    pruefidentifikator: str = Field(..., title="Pruefidentifikator")
+    datenaustauschreferenz: str = Field(..., title="Datenaustauschreferenz")
+    nachrichtendatum: str = Field(..., title="Nachrichtendatum")
+    nachrichten_referenznummer: str = Field(..., title="Nachrichten_referenznummer")
+    absender: str = Field(..., title="Absender")
+    empfaenger: str = Field(..., title="Empfaenger")
     lieferrichtung: str | None = Field(default=None, title="Lieferrichtung")
     referenznummer: str | None = Field(default=None, title="Referenznummer")
     duplikat: str = Field(..., title="Duplikat")
-    status: Any = Field(..., title="Status")
+    status: str = Field(..., title="Status")
